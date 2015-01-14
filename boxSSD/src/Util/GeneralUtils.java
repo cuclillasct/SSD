003a08db87e837bdb0afd8d737599b7566ec5261
elenamseo <elenamseoane@gmail.com>
2015-01-14 14:46:38 +0100

    Añadido método en utilidades para obtener los hash a partir de un String con la ruta de un archivo

--- boxSSD/src/Util/GeneralUtils.java
+++ boxSSD/src/Util/GeneralUtils.java
@@ -1,5 +1,10 @@
 package Util;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 import Models.DataChunk;
@@ -18,6 +23,36 @@ public class GeneralUtils {
 			}
 		}
 		return packs;
+	}
+	
+	
+	public byte[] getHash(String pathFile) throws IOException{
+	    MessageDigest md = null;
+	    FileInputStream file;
+	    byte[] hashBytes;
+	    
+		try {
+			md = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("No existe ese algoritmo de encriptado");
+			e.printStackTrace();
+		}
+		try {
+			file = new FileInputStream(pathFile);//coge el fichero seleccionado
+		    byte[] data = new byte[1024*100];
+			
+		    int nread = 0; //bytes leidos. almacenará -1 si no quedan bytes por leer
+		    while ((nread = file.read(data)) != -1) {//lee los bytes del tamaño de data y los almacena en él.
+		      md.update(data, 0, nread);	//actualiza con ese bloque de datos
+		    }
+		} catch (FileNotFoundException e) {
+			System.out.println("No se ha encontrado el archivo: " + pathFile);
+			e.printStackTrace();
+		}
+
+	    hashBytes = md.digest();//vuelco el hash en el array de bytes
+	    
+	    return hashBytes;
 	}
 
 }
